7a94635a5dd99319935a68ba0edf042d61733c86
xiajun003 <xiajun003@ke.com>
2018-12-20 16:56:51 +0800

    暂存

---
+++ car-web/src/main/java/com/aisino/StartApplication.java
@@ -0,0 +1,16 @@
+package com.aisino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: xiajun003
+ * @Date: 2018/12/18 16:31
+ * @Description:
+ */
+@SpringBootApplication
+public class StartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class,args);
+    }
+}
---
+++ car-web/src/main/java/com/aisino/guavaRatelimiter/RateLImiterController.java
@@ -0,0 +1,52 @@
+package com.aisino.guavaRatelimiter;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
+import com.google.common.util.concurrent.RateLimiter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * @author: xiajun003
+ * @Date: 2018/12/18 16:48
+ * @Description:
+ */
+@RestController
+public class RateLImiterController {
+
+    /**
+     * 指定每秒放两个令牌
+     */
+    RateLimiter ratelimiter = RateLimiter.create(2);
+
+    @RequestMapping("ratelimiter")
+    public String testGuavaRateLimiter(){
+        System.err.println(ratelimiter.getRate());
+        if (ratelimiter.tryAcquire()){
+            return "rate limiter get a lock";
+        }else {
+            return "not get lock";
+        }
+
+
+    }
+
+
+
+    @RequestMapping("cache")
+    public String testGuavaCache(String arg){
+        Cache<String , String> cache = CacheBuilder.newBuilder()
+                .expireAfterWrite(1, TimeUnit.SECONDS)
+                .maximumSize(1000).recordStats()
+                .removalListener(notification -> {
+
+                }).build();
+
+        return null;
+    }
+}
